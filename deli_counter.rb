57832ab8a9d7cb32f37016ed8bd3f0f1fbceac0f
Alethea Cary <aletheacary@gmail.com>
2019-05-22 20:49:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,8 @@ def take_a_number (katz_deli, name)
 end
 
 def now_serving (katz_deli)
-  if katz_deli != []
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
+  if katz_deli != []? (puts "Currently serving #{katz_deli.shift}."):(puts "There is nobody waiting to be served!")
   else
-    puts "There is nobody waiting to be served!"
+    
   end
 end
